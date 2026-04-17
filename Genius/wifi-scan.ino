7d8f94d03b5ed2575bfbd2f87de59eab3f79e8ae
Maicon Douglas <maicon.timot8@gmail.com>
2026-04-17 20:36:33 -0300

    Conecção MQTT

--- Genius/wifi-scan.ino
+++ Genius/wifi-scan.ino
@@ -1,6 +1,14 @@
+#include <WiFi.h>
 #include <Wire.h>
+#include <PubSubClient.h>
 #include <LiquidCrystal_I2C.h>
  
+const char* ssid = "Wokwi-GUEST";         
+const char* password = "";                
+const char* mqtt_server = "broker.hivemq.com";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
  
 const int inverm = 12; const int inverd = 14; const int inazul = 27; const int inamar = 26;
@@ -21,14 +29,53 @@ char tag[4] = {'A', 'A', 'A', '\0'};
 int tagIndex = 0;
  
  
+void setup_wifi() {
+  delay(10);
+  lcd.clear();
+  lcd.print("Conectando WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    lcd.setCursor(0, 1); lcd.print(".");
+  }
+  lcd.clear();
+  lcd.print("WiFi Online!");
+  delay(1000);
+}
+ 
+void reconnect() {
+  while (!client.connected()) {
+    String clientId = "ESP32Genius-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("Conectado ao MQTT");
+    } else {
+      delay(2000);
+    }
+  }
+}
+ 
+void publicarResultado() {
+  if (!client.connected()) {
+    reconnect();
+  }
+ 
+  String json = "{\"tag\": \"" + String(tag) + "\", \"score\": " + String(nivel) + "}";
+  
+  char attributes[100];
+  json.toCharArray(attributes, 100);
+  
+  client.publish("disruptive/genius/rank", attributes);
+  Serial.println("Resultado publicado: " + json);
+}
+ 
+ 
 void mostraCor(int cor, int duracao) {
   if(cor == 1) { digitalWrite(outverm, HIGH); tone(buzzer, 440); }
   else if(cor == 2) { digitalWrite(outverd, HIGH); tone(buzzer, 523); }
   else if(cor == 3) { digitalWrite(outazul, HIGH); tone(buzzer, 659); }
   else if(cor == 4) { digitalWrite(outamar, HIGH); tone(buzzer, 784); }
-  
   delay(duracao);
-  
   digitalWrite(outverm, LOW); digitalWrite(outverd, LOW);
   digitalWrite(outazul, LOW); digitalWrite(outamar, LOW);
   noTone(buzzer);
@@ -42,78 +89,49 @@ int leInput() {
   return 0;
 }
  
+ 
 void runStartup() {
-  lcd.clear();
-  lcd.setCursor(2, 0); lcd.print("INICIANDO");
-  lcd.setCursor(3, 1); lcd.print("GENIUS <3!");
-  delay(1500);
-  lcd.clear();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
   estadoAtual = TAG_SELECTION;
 }
  
 void runTagSelection() {
-  lcd.setCursor(0, 0);
-  lcd.print("Escolha sua TAG:");
-  
+  lcd.setCursor(0, 0); lcd.print("Escolha sua TAG:");
   lcd.setCursor(5, 1);
   for (int i = 0; i < 3; i++) {
-    if (i == tagIndex) {
-      lcd.print("["); lcd.print(tag[i]); lcd.print("]");
-    } else {
-      lcd.print(" "); lcd.print(tag[i]); lcd.print(" ");
-    }
+    if (i == tagIndex) { lcd.print("["); lcd.print(tag[i]); lcd.print("]"); }
+    else { lcd.print(" "); lcd.print(tag[i]); lcd.print(" "); }
   }
  
   int btn = leInput();
   if (btn != 0 && keyDown == 0) {
     keyDown = 1;
     tone(buzzer, 1000, 50);
- 
-    if (btn == 1) {
-      tag[tagIndex]++;
-      if (tag[tagIndex] > 'Z') tag[tagIndex] = 'A';
-    }
-    else if (btn == 2) {
-      tag[tagIndex]--;
-      if (tag[tagIndex] < 'A') tag[tagIndex] = 'Z';
-    }
-    else if (btn == 3) {
-      if (tagIndex > 0) {
-        tagIndex--;
-      }
-    }
+    if (btn == 1) { tag[tagIndex]++; if (tag[tagIndex] > 'Z') tag[tagIndex] = 'A'; }
+    else if (btn == 2) { tag[tagIndex]--; if (tag[tagIndex] < 'A') tag[tagIndex] = 'Z'; }
+    else if (btn == 3) { if (tagIndex > 0) tagIndex--; }
     else if (btn == 4) {
       tagIndex++;
       if (tagIndex >= 3) {
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Boa sorte!");
-        lcd.setCursor(0, 1);
-        lcd.print(tag);
-        delay(1500);
-        lcd.clear();
-        
+        lcd.clear(); lcd.print("Boa sorte, "); lcd.print(tag);
+        delay(1500); lcd.clear();
         lcd.setCursor(0, 0); lcd.print("Player: "); lcd.print(tag);
         lcd.setCursor(0, 1); lcd.print("Score: 0");
         estadoAtual = GAME;
       }
     }
-  }
-  else if (btn == 0) {
-    keyDown = 0;
-  }
+  } else if (btn == 0) { keyDown = 0; }
 }
  
 void runGame() {
   int tempoRodada = max(50, (int)(tempoBase * pow(0.9, nivel)));
-  int intervalo = tempoRodada / 2;
- 
   if(GameState == 0) {
     delay(800);
     cores[nivel] = random(1, 5);
     for(int i = 0; i <= nivel; i++) {
       mostraCor(cores[i], tempoRodada);
-      delay(intervalo);
+      delay(tempoRodada/2);
     }
     GameState = 1;
     atual = 0;
@@ -126,9 +144,7 @@ void runGame() {
       if(cor == cores[atual]) {
         atual++;
         if(atual > nivel) {
-          nivel++;
-          atual = 0;
-          GameState = 0;
+          nivel++; atual = 0; GameState = 0;
           lcd.setCursor(7, 1); lcd.print(nivel);
         }
       }
@@ -140,39 +156,21 @@ void runGame() {
  
 void runGameOver() {
   lcd.clear();
-  lcd.setCursor(3, 0); lcd.print("FIM DE JOGO");
-  lcd.setCursor(1, 1);
-  lcd.print(tag);
-  lcd.print(" - Score: ");
-  lcd.print(nivel);
+  lcd.setCursor(3, 0); lcd.print("GAME OVER!");
+  lcd.setCursor(1, 1); lcd.print(tag); lcd.print(" Score: "); lcd.print(nivel);
  
+  publicarResultado();
+  
   tone(buzzer, 150, 1000);
-  for(int i = 0; i < 5; i++) {
-    digitalWrite(outverm, HIGH); digitalWrite(outverd, HIGH);
-    digitalWrite(outazul, HIGH); digitalWrite(outamar, HIGH);
-    delay(200);
-    digitalWrite(outverm, LOW); digitalWrite(outverd, LOW);
-    digitalWrite(outazul, LOW); digitalWrite(outamar, LOW);
-    delay(200);
-  }
+  delay(4000);
  
-  delay(3000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Reiniciando...");
-  delay(1500);
- 
-  nivel = 0;
-  atual = 0;
-  GameState = 0;
-  tagIndex = 0;
+  nivel = 0; atual = 0; GameState = 0; tagIndex = 0;
   tag[0] = 'A'; tag[1] = 'A'; tag[2] = 'A';
-  
   estadoAtual = TAG_SELECTION;
 }
  
 void setup() {
+  Serial.begin(115200);
   lcd.init(); lcd.backlight();
   pinMode(inverm, INPUT_PULLDOWN); pinMode(inverd, INPUT_PULLDOWN);
   pinMode(inazul, INPUT_PULLDOWN); pinMode(inamar, INPUT_PULLDOWN);
@@ -184,6 +182,11 @@ void setup() {
 }
  
 void loop() {
+  if (!client.connected() && estadoAtual != STARTUP) {
+    reconnect();
+  }
+  client.loop();
+ 
   switch(estadoAtual) {
     case STARTUP: runStartup(); break;
     case TAG_SELECTION: runTagSelection(); break;
